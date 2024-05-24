288d7c1421ba4271ca005514b7d14992ae676a91
Hiram Guerrero <hiramg90@gmail.com>
2024-05-23 20:48:55 -0600

    section 3: aws cdk - intermediate topics

---
+++ lib/PhotosStack.ts
@@ -0,0 +1,33 @@
+import * as cdk from "aws-cdk-lib";
+import {Construct} from "constructs";
+import {Bucket} from "aws-cdk-lib/aws-s3";
+import {CfnOutput, Fn} from "aws-cdk-lib";
+
+export class PhotosStack extends cdk.Stack {
+
+  private stackSuffix: string;
+  public readonly photosBucketArn: string;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    this.initializeSuffix();
+
+    // If we replace the id every resource in this stack will be deleted and re-created
+    // this action may cause loss of data
+    const photosBucket = new Bucket(this, 'PhotosBucket2', {
+      bucketName: 'photos-bucket-' + this.stackSuffix
+    });
+
+    this.photosBucketArn = photosBucket.bucketArn;
+
+    // new CfnOutput(this, 'photos-bucket', {
+    //   value: photosBucket.bucketArn,
+    //   exportName: 'photos-bucket'
+    // });
+  }
+
+  private initializeSuffix() {
+    const shortStackId = Fn.select(2, Fn.split('/', this.stackId));
+    this.stackSuffix = Fn.select(4, Fn.split('-', shortStackId));
+  }
+}
